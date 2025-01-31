91ac61037e70fb21fba381a8a19b57410edf62a7
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2025-01-31 18:37:47 -0300

    Creating event OnClick in element Button

--- Projects/costsOfTravel/app/src/main/java/com/example/costsoftravel/MainActivity.kt
+++ Projects/costsOfTravel/app/src/main/java/com/example/costsoftravel/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.example.costsoftravel
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.costsoftravel.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater);
         setContentView(binding.root)
+        binding.buttonCalculate.setOnClickListener(this)
         //setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -25,7 +28,15 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun calculate() {
-        binding.
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculate()
+        }
     }
+
+    private fun calculate() {
+        Toast.makeText(this, "Fui clicado", Toast.LENGTH_SHORT).show()
+    }
+
+
 }
