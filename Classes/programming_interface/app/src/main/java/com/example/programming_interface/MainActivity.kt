5f1162fc6e56b84d06cabd64208011f5d1578efc
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2025-05-26 11:06:09 -0300

    New mapping elements with viewBinding

--- Classes/programming_interface/app/src/main/java/com/example/programming_interface/MainActivity.kt
+++ Classes/programming_interface/app/src/main/java/com/example/programming_interface/MainActivity.kt
@@ -2,22 +2,22 @@ package com.example.programming_interface
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.programming_interface.ui.theme.Programming_interfaceTheme
+import com.example.programming_interface.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.edittextEmail.text.toString()
         }
+
     }
 
