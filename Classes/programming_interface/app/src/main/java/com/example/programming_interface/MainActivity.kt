6d5e863875e4a878a6e4abd0e3ed2d2142d69384
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2025-05-30 10:15:17 -0300

    Navegation between screens

--- Classes/programming_interface/app/src/main/java/com/example/programming_interface/MainActivity.kt
+++ Classes/programming_interface/app/src/main/java/com/example/programming_interface/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.programming_interface
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+
 import com.example.programming_interface.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +20,29 @@ class MainActivity : ComponentActivity() {
 
         setContentView(binding.root)
 
-        binding.edittextEmail.text.toString()
+        binding.buttonLogin.setOnClickListener(this)
+        binding.buttonRegister.setOnClickListener(this)
+
         }
+
+    override fun onClick(v: View) {
+        if(v.id == R.id.button_login) {
+            val email = binding.edittextEmail.text.toString()
+            val password = binding.editTextPassword.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()) {
+                val intent = Intent(this, HomeActivity::class.java)
+
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show()
+            }
+
+        }else if(v.id == R.id.button_register) {
+            println()
+        }
+    }
 
     }
 
