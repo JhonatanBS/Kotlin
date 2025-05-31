037254568e917ce5976ff022601cd302c61aa473
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2025-05-31 19:17:32 -0300

    Creating screen home

--- Classes/RelativeLayout/app/src/main/java/com/example/relativelayout/HomeActivity.kt
+++ Classes/RelativeLayout/app/src/main/java/com/example/relativelayout/HomeActivity.kt
@@ -6,17 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left + 16,
-                systemBars.top + 16,
-                systemBars.right + 16,
-                systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
