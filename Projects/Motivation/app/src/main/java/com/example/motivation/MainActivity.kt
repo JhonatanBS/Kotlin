ce9ffcaeea92df7b77de181a33d9b069f30d15e3
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2025-06-03 19:52:36 -0300

    Creating project Motivation and home screen

---
+++ Projects/Motivation/app/src/main/java/com/example/motivation/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.motivation
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.motivation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate((layoutInflater))
+
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom)
+            insets
+        }
+
+        setListeners()
+    }
+
+    override fun onClick(v: View) {
+        if(v.id == R.id.button_newPhrase) {
+            handleNewPhrase()
+        }
+    }
+
+    private fun handleNewPhrase() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setListeners() {
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+
+}
