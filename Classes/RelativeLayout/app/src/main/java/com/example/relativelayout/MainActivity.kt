4a704442e56a58f678d561af9b4f539727b25893
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2025-06-03 17:02:55 -0300

    Project RelativeLayout Finished

--- Classes/RelativeLayout/app/src/main/java/com/example/relativelayout/MainActivity.kt
+++ Classes/RelativeLayout/app/src/main/java/com/example/relativelayout/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.relativelayout
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.relativelayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left + 16,
@@ -18,6 +26,17 @@ class MainActivity : AppCompatActivity() {
                 systemBars.right + 16,
                 systemBars.bottom)
             insets
+        }
+
+        val buttonLogin = binding.buttonLogin
+        val email = binding.edittextEmail.text.toString()
+        val password = binding.edittextPassword.text.toString()
+
+        buttonLogin.setOnClickListener{
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.putExtra("EMAIL", email)
+            intent.putExtra("PASSWORD", password)
+            startActivity(intent)
         }
     }
 }
