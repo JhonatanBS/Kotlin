0c8ca6235b36797e575559084bcc8b1ef2cc5257
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2025-10-01 19:56:38 -0300

    Creating logic for changing the listing layout

--- Projects/mybooks/app/src/main/java/com/example/mybooks/ui/viewholder/BookViewHolder.kt
+++ Projects/mybooks/app/src/main/java/com/example/mybooks/ui/viewholder/BookViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.mybooks.ui.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mybooks.R
 import com.example.mybooks.databinding.ItemBookBinding
 import com.example.mybooks.entity.BookEntity
 
@@ -10,5 +11,33 @@ class BookViewHolder(private val item: ItemBookBinding) : RecyclerView.ViewHolde
         item.textviewTitle.text = book.title
         item.textviewAuthor.text = book.author
         item.textviewGenre.text = book.genre
+
+        setGenreBackgroundColor(book.genre)
+
+        updateFavoriteIcon(book.favorite)
+    }
+
+    private fun updateFavoriteIcon(favorite: Boolean) {
+        if (favorite) {
+            item.imageviewFavorite.setImageResource(R.drawable.ic_favorite)
+        } else {
+            item.imageviewFavorite.setImageResource(R.drawable.ic_favorite_empty)
+        }
+    }
+
+    private fun setGenreBackgroundColor(genre: String) {
+        when (genre) {
+            "Terror" -> {
+                item.textviewGenre.setBackgroundResource(R.drawable.rounded_label_red)
+            }
+
+            "Fantasia" -> {
+                item.textviewGenre.setBackgroundResource(R.drawable.rounded_label_fantasy)
+            }
+
+            else -> {
+                item.textviewGenre.setBackgroundResource(R.drawable.rounded_label_teal)
+            }
+        }
     }
 }
