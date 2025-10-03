2216ad99b293095373ae90a6daa80f6df8db919e
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2025-10-03 09:45:28 -0300

    Creating click event for navigation between fragments

--- Projects/mybooks/app/src/main/java/com/example/mybooks/ui/HomeFragment.kt
+++ Projects/mybooks/app/src/main/java/com/example/mybooks/ui/HomeFragment.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mybooks.R
 import com.example.mybooks.databinding.FragmentHomeBinding
 import com.example.mybooks.ui.adapter.BookAdapter
-import com.example.mybooks.viewmodels.HomeViewModel
+import com.example.mybooks.ui.listener.BookListener
+import com.example.mybooks.viewmodel.HomeViewModel
 
 class HomeFragment : Fragment() {
 
@@ -35,16 +37,26 @@ class HomeFragment : Fragment() {
         // Adapter
         binding.recyclerviewBooks.adapter = adapter
 
+        attachListener()
+
         viewModel.getAllBooks()
+        setObservers()
 
-
-setObservers()
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun attachListener() {
+        adapter.attachListener(object: BookListener {
+            override fun onClick(id: Int) {
+                findNavController().navigate(R.id.navigation_details)
+            }
+
+        })
     }
 
     private fun setObservers() {
--- Projects/mybooks/app/src/main/java/com/example/mybooks/ui/adapter/BookAdapter.kt
+++ Projects/mybooks/app/src/main/java/com/example/mybooks/ui/adapter/BookAdapter.kt
@@ -5,18 +5,20 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mybooks.databinding.ItemBookBinding
 import com.example.mybooks.entity.BookEntity
+import com.example.mybooks.ui.listener.BookListener
 import com.example.mybooks.ui.viewholder.BookViewHolder
 
 class BookAdapter : RecyclerView.Adapter<BookViewHolder>(){
 
     private var bookList: List<BookEntity> = listOf()
+    private lateinit var  bookListener: BookListener
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): BookViewHolder {
         val view = ItemBookBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-       return BookViewHolder(view)
+       return BookViewHolder(view, bookListener)
     }
 
     override fun onBindViewHolder(
@@ -33,5 +35,9 @@ class BookAdapter : RecyclerView.Adapter<BookViewHolder>(){
     fun updateBooks(list: List<BookEntity>) {
         bookList = list
         notifyDataSetChanged()
+    }
+
+    fun attachListener(listener: BookListener) {
+        bookListener = listener
     }
 }
