4b88bd69987b9c6d3e7b2247eab4da3025b6789c
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2025-10-07 13:55:29 -0300

    Creating a listing of favorite

--- Projects/mybooks/app/src/main/java/com/example/mybooks/ui/FavoriteFragment.kt
+++ Projects/mybooks/app/src/main/java/com/example/mybooks/ui/FavoriteFragment.kt
@@ -6,7 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mybooks.R
 import com.example.mybooks.databinding.FragmentFavoriteBinding
+import com.example.mybooks.helper.BookConstants
+import com.example.mybooks.ui.adapter.BookAdapter
+import com.example.mybooks.ui.listener.BookListener
 import com.example.mybooks.viewmodel.FavoriteViewModel
 
 class FavoriteFragment : Fragment() {
@@ -15,6 +21,7 @@ class FavoriteFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: FavoriteViewModel by viewModels()
+    private val adapter: BookAdapter = BookAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,11 +31,48 @@ class FavoriteFragment : Fragment() {
 
         _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
 
+        binding.recyclerviewBooksFavorite.layoutManager = LinearLayoutManager(context)
+
+        // Adapter
+        binding.recyclerviewBooksFavorite.adapter = adapter
+
+        attachListener()
+
+        setObservers()
+
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getFavoriteBooks()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun attachListener() {
+        adapter.attachListener(object : BookListener {
+            override fun onClick(id: Int) {
+                val bundle = Bundle()
+                bundle.putInt(BookConstants.KEY.BOOK_ID, id)
+
+                findNavController().navigate(R.id.navigation_details, bundle)
+            }
+
+            override fun onFavoriteClick(id: Int) {
+                viewModel.favorite(id)
+                viewModel.getFavoriteBooks()
+            }
+
+        })
+    }
+
+    private fun setObservers() {
+        viewModel.books.observe(viewLifecycleOwner) {
+            adapter.updateBooks((it))
+        }
     }
 }
