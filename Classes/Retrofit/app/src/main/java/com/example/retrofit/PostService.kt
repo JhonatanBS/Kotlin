66b730d41439e0e0baa34ca0623dadc0789dade5
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2025-11-20 08:37:36 -0300

    Creating Retrofit layer with coroutines

--- Classes/Retrofit/app/src/main/java/com/example/retrofit/PostService.kt
+++ Classes/Retrofit/app/src/main/java/com/example/retrofit/PostService.kt
@@ -1,10 +1,14 @@
 package com.example.retrofit
 
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface PostService {
-    
+
     @GET("posts")
     fun list(): Call<List<PostEntity>>
+
+    @GET("posts")
+    suspend fun listCoroutine(): Response<List<PostEntity>>
 }
---
+++ Classes/Retrofit/app/src/main/java/com/example/retrofit/RetrofitClient.kt
@@ -0,0 +1,31 @@
+package com.example.retrofit
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClient private constructor(){
+
+    companion object {
+
+        private lateinit var instance: Retrofit
+
+        private fun getRetrofitInstance(): Retrofit {
+            val httpClient = OkHttpClient.Builder()
+            synchronized(this) {
+                if(!::instance.isInitialized) {
+                    instance = Retrofit.Builder()
+                        .client(httpClient.build())
+                        .baseUrl("https://jsonplaceholder.typicode.com/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build()
+                }
+            }
+            return instance
+        }
+
+        fun <S> createService(serviceClass: Class<S>): S {
+            return getRetrofitInstance().create(serviceClass)
+        }
+    }
+}
