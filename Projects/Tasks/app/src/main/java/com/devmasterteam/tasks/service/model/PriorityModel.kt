96039bb5d16309e667bc51d95be9465a45e92b51
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2026-01-21 16:10:54 -0300

    Service - Priorities and Tasks

--- Projects/Tasks/app/src/main/java/com/devmasterteam/tasks/service/model/PriorityModel.kt
+++ Projects/Tasks/app/src/main/java/com/devmasterteam/tasks/service/model/PriorityModel.kt
@@ -1,4 +1,19 @@
 package com.devmasterteam.tasks.service.model
 
-class PriorityModel {
-}
+import com.google.gson.annotations.SerializedName
+
+data class PriorityModel (
+
+    @SerializedName("Id")
+    val id: Int,
+
+    @SerializedName("Description")
+    val description: String
+)
+
+/*
+*  {
+        "Id": 1,
+        "Description": "Baixa"
+    },
+* */
