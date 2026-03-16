7a581bf7be38481a3639417d34ba20a3a2309bd2
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2026-03-16 17:52:20 -0300

    Components: Spinner Dynamics

--- Classes/Components/app/src/main/java/com/example/components/MainActivity.kt
+++ Classes/Components/app/src/main/java/com/example/components/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.components
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.components.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener, AdapterView.OnItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +21,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         binding.buttonToast.setOnClickListener(this)
         binding.buttonSnack.setOnClickListener(this)
+
+        binding.buttonGetSpinner.setOnClickListener(this)
+        binding.buttonSetSpinner.setOnClickListener(this)
+        binding.spinnerDinamico.onItemSelectedListener = this
+
+        // loadSpinner()
     }
 
     override fun onClick(v: View) {
@@ -43,6 +51,45 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
                 snack.show()
             }
+
+            R.id.button_get_spinner -> {
+
+                loadSpinner()
+
+                /*  val str = binding.spinnerDinamico.selectedItem
+
+                  val id1 = binding.spinnerDinamico.selectedItemId
+                  val id2 = binding.spinnerDinamico.selectedItemPosition
+                */
+            }
+
+            R.id.button_set_spinner -> {
+                //binding.spinnerDinamico.setSelection(2)
+                binding.spinnerDinamico.adapter = null
+            }
         }
     }
+
+    override fun onItemSelected(
+        parent: AdapterView<*>,
+        view: View,
+        position: Int,
+        id: Long
+    ) {
+        Toast.makeText(this, "$position - $id", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        Toast.makeText(this, "Nothing", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun loadSpinner() {
+        val list = listOf("Gramas", "Kg", "Arroba", "Tonelada")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, list)
+
+        binding.spinnerDinamico.adapter = adapter
+    }
+
+
 }
