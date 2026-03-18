1bfb0f965a407e19f52b12044c67f7897d6d5f69
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2026-03-18 09:28:52 -0300

    Components: DatePickerDialog

--- Classes/Components/app/src/main/java/com/example/components/TimeActivity.kt
+++ Classes/Components/app/src/main/java/com/example/components/TimeActivity.kt
@@ -1,13 +1,14 @@
 package com.example.components
 
-import android.app.TimePickerDialog
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.View
-import android.widget.TimePicker
+import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
 import com.example.components.databinding.ActivityTimeBinding
 
-class TimeActivity : AppCompatActivity(), View.OnClickListener, TimePickerDialog.OnTimeSetListener {
+class TimeActivity : AppCompatActivity(), View.OnClickListener, DatePickerDialog.OnDateSetListener {
     private lateinit var binding: ActivityTimeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,22 +17,29 @@ class TimeActivity : AppCompatActivity(), View.OnClickListener, TimePickerDialog
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        binding.buttonTimePicker.setOnClickListener(this)
+        binding.buttonDatePicker.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
         when (v.id) {
-            R.id.button_time_picker -> {
-                TimePickerDialog(this, this, 20, 15, true).show()
+            R.id.button_date_picker -> {
+                val dateNow = Calendar.getInstance()
+                val year = dateNow.get(Calendar.YEAR)
+                val month = dateNow.get(Calendar.MONTH)
+                val dayOfMonth = dateNow.get(Calendar.DAY_OF_MONTH)
+
+                DatePickerDialog(this, this, year, month, dayOfMonth).show()
             }
         }
     }
 
-    override fun onTimeSet(
-        view: TimePicker,
-        hourOfDay: Int,
-        minute: Int
+    override fun onDateSet(
+        view: DatePicker?,
+        year: Int,
+        month: Int,
+        dayOfMonth: Int
     ) {
-        binding.textTimePicker.text = "$hourOfDay:$minute"
+        binding.textDatePicker.text = "$dayOfMonth/${month + 1}/$year"
     }
+
 }
