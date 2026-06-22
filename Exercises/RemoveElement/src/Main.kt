6d5f526f53468b3e7786d103e8f84ed9747ff6d5
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2026-06-22 19:01:33 -0300

    exercise: Remove Element

---
+++ Exercises/RemoveElement/src/Main.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+    println(RemoveElement().solution(intArrayOf(3,2,2,3), 3))
+    println(RemoveElement().solution(intArrayOf(0,1,2,2,3,0,4,2), 2))
+}
---
+++ Exercises/RemoveElement/src/RemoveElement.kt
@@ -0,0 +1,14 @@
+class RemoveElement {
+    fun solution(nums: IntArray, `val`: Int): Int {
+        var k = 0
+
+        for (i in nums.indices) {
+            if (nums[i] != `val`) {
+                nums[k] = nums[i]
+                k++
+            }
+        }
+
+        return k
+    }
+}
